b8422485b96787b7d5b74cb7e3a1cd4766f9ffce
Vinod <vinodyuvasoft145@gmail.com>
2017-07-05 17:05:21 +0530

    login by facebook & remaining changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,6 @@
+//= require jquery
 //= require rails-ujs
 //= require turbolinks
+//= require gmaps/google
 //= require bootstrap-sprockets
+//= require_tree .
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:age, :gender])
+      devise_parameter_sanitizer.permit(:account_update) do |u| 
+        u.permit(
+          :current_password,
+          :email, 
+          :first_name,
+          :last_name,
+          :age,
+          :address, 
+          :gender,
+          :phone)
+      end
+    end 
+
 end
